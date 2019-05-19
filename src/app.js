0b812cf83696539f8c447879e723fdaf0b64cfd8
Russell <russelldoe@hotmail.co.uk>
2019-05-19 20:10:24 +0100

    first character complete

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const { sayHello, uppercase, lowercase } = require('./lib/strings');
+const { sayHello, uppercase, lowercase, firstCharacter } = require('./lib/strings');
 
 // app.get('/strings/hello/world', (req, res) => {
 //   res.status(200).json({ result: 'Hello world!' });
@@ -17,6 +17,10 @@ app.get('/strings/upper/:string', (req, res) => {
 
 app.get('/strings/lower/:string', (req, res) => {
   res.json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  res.json({ result: firstCharacter(req.params.string) });
 });
 
 module.exports = app;
